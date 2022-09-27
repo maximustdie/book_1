5aac8e9f8e28d2ffbc5ffbe4b09f28c61cba152e
maximustdie <maksimsimkinv@gmail.com>
2022-09-28 01:46:07 +0300

    Изменены настройки БД в settings.py

--- books/books/settings.py
+++ books/books/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'books.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbpostgresql',
+        'USER': 'admin',
+        'PASSWORD': 'R2qla6Msg789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
