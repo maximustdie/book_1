7701c3d9a399a50a0bfd0c5c0801b5afa1a96fa5
maximustdie <maksimsimkinv@gmail.com>
2022-09-28 10:08:44 +0300

    Добавлен класс UserManager в models.py

--- books/book_auth/models.py
+++ books/book_auth/models.py
@@ -1,3 +1,40 @@
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """ Класс Manager для кастомных классов пользователей """
+
+    def create_user(self, username, email, password=None):
+        """ Создает и возвращает пользователя с имэйлом, паролем и именем. """
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """ Создает и возввращет пользователя с привилегиями суперадмина. """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
