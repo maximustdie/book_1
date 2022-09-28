a0a11649b4ed2cfb0c6525ea12bb19af4f857e43
maximustdie <maksimsimkinv@gmail.com>
2022-09-28 10:16:36 +0300

    Добавлен класс User в models.py

--- books/book_auth/models.py
+++ books/book_auth/models.py
@@ -16,10 +16,10 @@ class UserManager(BaseUserManager):
     def create_user(self, username, email, password=None):
         """ Создает и возвращает пользователя с имэйлом, паролем и именем. """
         if username is None:
-            raise TypeError('Users must have a username.')
+            raise TypeError('Пользователь должен иметь логин.')
 
         if email is None:
-            raise TypeError('Users must have an email address.')
+            raise TypeError('Пользователь должен иметь пароль.')
 
         user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
@@ -28,9 +28,9 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password):
-        """ Создает и возввращет пользователя с привилегиями суперадмина. """
+        """ Создает и возвращает пользователя с привилегиями суперадмина. """
         if password is None:
-            raise TypeError('Superusers must have a password.')
+            raise TypeError('Суперпользователь должен иметь пароль.')
 
         user = self.create_user(username, email, password)
         user.is_superuser = True
@@ -38,3 +38,55 @@ class UserManager(BaseUserManager):
         user.save()
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        """ Строковое представление модели """
+        return self.email
+
+    @property
+    def token(self):
+        """
+        Позволяет получить токен пользователя путем вызова user.token, вместо
+        user._generate_jwt_token(). 
+        """
+        return self._generate_jwt_token()
+
+    def get_full_name(self):
+        """
+        Этот метод требуется Django для таких вещей, как обработка электронной
+        почты. Обычно это имя фамилия пользователя, но поскольку мы не
+        используем их, будем возвращать username.
+        """
+        return self.username
+
+    def get_short_name(self):
+        """ Аналогично методу get_full_name(). """
+        return self.username
+
+    def _generate_jwt_token(self):
+        """
+        Генерирует веб-токен JSON, в котором хранится идентификатор этого
+        пользователя, срок действия токена составляет 1 день от создания
+        """
+        dt = datetime.now() + timedelta(days=1)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token.decode('utf-8')
