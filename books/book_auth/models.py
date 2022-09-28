b8030996e445b2351991ea999a5a18d5126f3228
maximustdie <maksimsimkinv@gmail.com>
2022-09-28 15:59:59 +0300

    Исправлена ошибка при входе в систему

--- books/book_auth/models.py
+++ books/book_auth/models.py
@@ -88,5 +88,4 @@ class User(AbstractBaseUser, PermissionsMixin):
             'id': self.pk,
             'exp': int(dt.strftime('%S'))
         }, settings.SECRET_KEY, algorithm='HS256')
-
-        return token.decode('utf-8')
+        return token
