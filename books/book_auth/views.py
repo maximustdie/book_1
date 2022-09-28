8d9a2c541b0149924463522bbb9025bb20f09d7f
maximustdie <maksimsimkinv@gmail.com>
2022-09-28 15:03:13 +0300

    Добавлен класс LoginAPIView в books/book_auth/views.py

--- books/book_auth/views.py
+++ books/book_auth/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .renderers import UserJSONRenderer
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
 
 
 class RegistrationAPIView(APIView):
@@ -21,3 +21,17 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+    renderer_classes = (UserJSONRenderer,)
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
