b4e5e38fdeef4c740d3cefe18fa6d7032ec0fcd7
maximustdie <maksimsimkinv@gmail.com>
2022-09-30 16:37:38 +0300

    Модели добвленны к админке

--- books/book_talk/admin.py
+++ books/book_talk/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Author, Book, Comment
+
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Comment)
