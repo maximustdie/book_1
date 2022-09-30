8173bcf3c50ea1e4f78ad493cadefad8e4c0c600
maximustdie <maksimsimkinv@gmail.com>
2022-09-30 13:45:22 +0300

    Добавлена модель Book

--- books/book_talk/models.py
+++ books/book_talk/models.py
@@ -18,7 +18,7 @@ class Book(models.Model):
     title = models.CharField(max_length=60, verbose_name='Название')
     annotation = models.TextField(verbose_name='Аннотация')
     author = models.ManyToManyField(Author, verbose_name='Автор книги', related_name='books')
-    creator_id = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True, verbose_name='Пользователь')
+    creator_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, verbose_name='Пользователь')
     archived = models.BooleanField(default=False, verbose_name='Отправлено в архив')
 
     class Meta:
@@ -27,3 +27,19 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Автор комментария',
+                               related_name='comments')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name='Книга', related_name='comments')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Дата создания: {self.create_time}, Автор комментария:{self.author} Книга:{self.book}'
