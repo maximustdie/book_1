3d43e2d6e3baf22ef6a897377c80490ccc255264
maximustdie <maksimsimkinv@gmail.com>
2022-10-03 14:26:54 +0300

    Добавлена возможность автоматического определения owner

--- books/book_talk/views.py
+++ books/book_talk/views.py
@@ -23,6 +23,7 @@ class AuthorList(APIView):
 
     def post(self, request, format=None):
         serializer = AuthorSerializer(data=request.data)
+        serializer.initial_data['owner'] = self.request.user.id
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -76,7 +77,9 @@ class BookList(APIView):
 
     def post(self, request, format=None):
         serializer = BookSerializer(data=request.data)
+        serializer.initial_data['owner'] = self.request.user.id
         if serializer.is_valid():
+
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -114,4 +117,4 @@ class BookDetail(APIView):
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    
+
