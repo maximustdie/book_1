5aba4dc3a10e9416634eb88a8feb5d58119762c6
maximustdie <maksimsimkinv@gmail.com>
2022-10-03 17:20:39 +0300

    Настроена админка

--- books/book_talk/admin.py
+++ books/book_talk/admin.py
@@ -5,6 +5,23 @@ from django.contrib import admin
 from .models import Author, Book, Comment
 
 
-admin.site.register(Author)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date', 'death_date', 'id')
+
+    @admin.display(description='Полное имя')
+    def full_name(self, obj):
+        return f'{obj.first_name} {obj.last_name} {obj.second_name}'
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_authors')
+    filter_horizontal = ['author']
+
+    @admin.display(description='Автор')
+    def get_authors(self, obj):
+        return ", ".join([f'{a.first_name} {a.last_name}' for a in obj.author.all()])
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Comment)
--- books/book_talk/urls.py
+++ books/book_talk/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from book_talk.views import AuthorList, AuthorDetail, BookList, BookDetail
+from book_talk.views import AuthorList, BookList, BookDetail
 
 app_name = 'book_talk'
 urlpatterns = [
-    path('author/<int:author_id>', AuthorDetail.as_view()),
+    # path('author/<int:author_id>', AuthorDetail.as_view()),
     path('author/', AuthorList.as_view()),
     path('book/<int:book_id>', BookDetail.as_view()),
     path('book/', BookList.as_view()),
