852a89fc9b8ea28739860d753b410d1fe6f43a89
maximustdie <maksimsimkinv@gmail.com>
2022-10-10 17:16:07 +0300

    Добавлено ПодключеннÐенÐа функция загрузки книги на сервер

--- books/book_files/urls.py
+++ books/book_files/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
+from book_files.views import FileView
 
 app_name = 'book_files'
 urlpatterns = [
-
+    path('upload/', FileView.as_view())
 ]
--- books/book_files/views.py
+++ books/book_files/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import FileSerializer
 
-# Create your views here.
+
+class FileView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
