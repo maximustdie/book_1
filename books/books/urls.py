db8097ea3081960ae2015979e9f9e4835a573d04
maximustdie <maksimsimkinv@gmail.com>
2022-10-10 16:30:01 +0300

    Добавлено Подключено приложение book_files

--- books/books/urls.py
+++ books/books/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
-
 schema_view = get_schema_view(openapi.Info(
     title="Book API",
     default_version='v1',
@@ -15,6 +14,7 @@ schema_view = get_schema_view(openapi.Info(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('book_auth.urls', namespace='book_auth')),
-    path('api/', include('book_talk.urls', namespace='book_auth')),
+    path('api/files/', include('book_files.urls', namespace='book_files')),
+    path('api/', include('book_talk.urls', namespace='book_talk')),
     path('doc_api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
